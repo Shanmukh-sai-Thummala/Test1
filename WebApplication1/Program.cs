2ef7b0b261712a46176357d89e70fed69a54cc54
Shanmukh-sai-Thummala <reachme.shanmukh@gmail.com>
2025-02-17 16:15:57 -0500

    Test

--- WebApplication1/Program.cs
+++ WebApplication1/Program.cs
@@ -3,6 +3,7 @@ namespace WebApplication1
 {
     public class Program
     {
+        //
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
--- WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
+++ WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+11c3785788faa568a0953c620bf99bad3acfd2c7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b20b0127962aa6b59da0f92e346f5fdb7f93a8ed")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
